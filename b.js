d300db1d02e161bee7ffca98e458fae21285294f
Hemanthdommeti <144368957+Hemanthdommeti@users.noreply.github.com>
2024-11-03 17:01:19 +0530

    Update b.js

--- b.js
+++ b.js
@@ -1,20 +1,68 @@
-// Todo: Make M+ M- and MC functional
-let string = "";
-let buttons = document.querySelectorAll('.button');
-Array.from(buttons).forEach((button)=>{
-  button.addEventListener('click', (e)=>{
-    if(e.target.innerHTML == '='){
-      string = eval(string);
-      document.querySelector('input').value = string;
-    }
-    else if(e.target.innerHTML == 'C'){
-      string = ""
-      document.querySelector('input').value = string;
-    }
-    else{ 
-    console.log(e.target)
-    string = string + e.target.innerHTML;
-    document.querySelector('input').value = string;
-      }
-  })
-})
+*{
+  margin:0;
+  padding:0;
+}
+@import url('https://fonts.googleapis.com/css2?family=Nerko+One&display=swap');
+.intro{
+  text-align: center;
+  font-family:'Nerko One', cursive;
+  background-color: rebeccapurple;
+  color:white;
+  font-size: xx-large;
+  margin:5px 0px;
+  padding:15px;
+}
+.button{
+  padding: 20px;
+  margin: 1px 0.5px;
+  border-radius: 22px;
+  font-size: medium;
+  background:#222222;
+  border: 1.5px solid black;
+  
+  color: aquamarine;
+
+}
+.Row{
+   margin: 2.5px 0px;
+ }
+.input{
+  font-size: 17.5px;
+  margin: 2px;
+  padding: 6px;
+  border: 3px solid black;
+  border-radius: 5px;
+}
+
+.flex{
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+}
+
+.AC{
+  padding: 20px 50px;
+}
+.DEL{
+  padding:20px 35px;
+    
+}
+.equal{
+  padding:20px 26px;
+} 
+
+.bgcolor{
+  background: #e3f9ff;
+  height:100vh;
+  display: flex;
+  justify-content: flex-start;
+  padding: 15px;
+ 
+  
+}
+
+
+
+
+
